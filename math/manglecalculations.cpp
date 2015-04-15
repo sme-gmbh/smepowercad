56b848894d491ed4da0925668c612eb55de59300
Sebastian Wolf <wolf.git@sme-gmbh.com>
2015-04-15 14:52:02 +0200

    Fixed bugs in anglesFromMatrix, all exceptions are properly dealt with now

--- math/manglecalculations.cpp
+++ math/manglecalculations.cpp
@@ -13,8 +13,10 @@ MAngleCalculations::~MAngleCalculations()
 //calculates a set of kardan angles, which describe the same rotation as given by matrix_rotation
 QVector3D MAngleCalculations::anglesFromMatrix(QMatrix4x4 matrix_rotation)
 {
+//    qDebug() << "original: " << matrix_rotation;
     qreal r11 = matrix_rotation.row(0).x();
     qreal r21 = matrix_rotation.row(1).x();
+    qreal r22 = matrix_rotation.row(1).y();
     qreal r31 = matrix_rotation.row(2).x();
     qreal r12 = matrix_rotation.row(0).y();
     qreal r13 = matrix_rotation.row(0).z();
@@ -25,55 +27,71 @@ QVector3D MAngleCalculations::anglesFromMatrix(QMatrix4x4 matrix_rotation)
 
     if(qAbs(r13 - 1) < EPS || qAbs(r13 + 1) < EPS)
     {
-        phi = 0.0;
-        qreal delta = atan2(r21, r31);
         if(qAbs(r13 + 1) < EPS)
         {
-            qDebug() << "anglesFromMatrix case 1";
-            theta = PI / 2.0;
-            psi = phi + delta;
-        }
-        else
-        {
-            qDebug() << "anglesFromMatrix case 2";
+//            qDebug() << "anglesFromMatrix case 1";
+            phi = 0.0;
+            qreal delta = atan2(r21, r31);
             theta = -PI / 2.0;
-            psi = - phi + delta;
+            psi = - phi - delta;
+            QMatrix4x4 m1;
+            m1.setToIdentity();
+            m1.rotate(psi / PI * 180.0,   1.0, 0.0, 0.0);
+            m1.rotate(theta / PI * 180.0, 0.0, 1.0, 0.0);
+            m1.rotate(phi / PI * 180.0,   0.0, 0.0, 1.0);
+//            qDebug() << "m1: " << m1;
+        }
+        if(qAbs(r13 - 1) < EPS)
+        {
+//            qDebug() << "anglesFromMatrix case 2";
+//            qDebug() << "Theta = 90 oder Theta = -270";
+            phi = 0.0;
+            qreal theta2 = PI / 2.0;
+            qreal psi2 = atan2(r21 , r22);
+            QMatrix4x4 m2;
+            m2.setToIdentity();
+            m2.rotate(psi2 / PI * 180.0,   1.0, 0.0, 0.0);
+            m2.rotate(theta2 / PI * 180.0, 0.0, 1.0, 0.0);
+            m2.rotate(phi / PI * 180.0,   0.0, 0.0, 1.0);
+//            qDebug() << "m2: " << m2;
+            theta = theta2;
+            psi = psi2;
         }
     }
     else
     {
-        qDebug() << "anglesFromMatrix case 3";
+//        qDebug() << "anglesFromMatrix case 3";
 
-        theta =  asin(r13);
-        //qreal theta2 = -PI + theta1;
-        psi = -atan2(r23 / cos(theta), r33 / cos(theta));
-        //qreal psi2 = -atan2(r23 / cos(theta2), r33 / cos(theta2));
-        phi = -atan2(r12 / cos(theta), r11 / cos(theta));
-        //qreal phi2 = -atan2(r12 / cos(theta2), r11 / cos(theta2));
-//        QMatrix4x4 m1;
-//        m1.setToIdentity();
-//        m1.rotate(psi1 / PI * 180.0,   1.0, 0.0, 0.0);
-//        m1.rotate(theta1 / PI * 180.0, 0.0, 1.0, 0.0);
-//        m1.rotate(phi1 / PI * 180.0,   0.0, 0.0, 1.0);
-//        QMatrix4x4 m2;
-//        m2.setToIdentity();
-//        m2.rotate(psi2 / PI * 180.0,   1.0, 0.0, 0.0);
-//        m2.rotate(theta2 / PI * 180.0, 0.0, 1.0, 0.0);
-//        m2.rotate(phi2 / PI * 180.0,   0.0, 0.0, 1.0);
+        qreal theta1 = asin(r13);
+        qreal theta2 = PI - theta1;
+        qreal psi1 = -atan2(r23 / cos(theta1), r33 / cos(theta1));
+        qreal psi2 = -atan2(r23 / cos(theta2), r33 / cos(theta2));
+        qreal phi1 = -atan2(r12 / cos(theta1), r11 / cos(theta1));
+        qreal phi2 = -atan2(r12 / cos(theta2), r11 / cos(theta2));
+        QMatrix4x4 m1;
+        m1.setToIdentity();
+        m1.rotate(psi1 / PI * 180.0,   1.0, 0.0, 0.0);
+        m1.rotate(theta1 / PI * 180.0, 0.0, 1.0, 0.0);
+        m1.rotate(phi1 / PI * 180.0,   0.0, 0.0, 1.0);
+        QMatrix4x4 m2;
+        m2.setToIdentity();
+        m2.rotate(psi2 / PI * 180.0,   1.0, 0.0, 0.0);
+        m2.rotate(theta2 / PI * 180.0, 0.0, 1.0, 0.0);
+        m2.rotate(phi2 / PI * 180.0,   0.0, 0.0, 1.0);
 //        qDebug() << "m1: " << m1;
-//        qDebug() << "original: " << matrix_rotation;
-//        if(matrixNorm(matrix_rotation - m1) < matrixNorm(matrix_rotation - m2))
-//        {
-//            psi = psi1;
-//            theta = theta1;
-//            phi = phi1;
-//        }
-//        else
-//        {
-//            psi = psi2;
-//            theta = theta2;
-//            phi = phi2;
-//        }
+//        qDebug() << "m2: " << m2;
+        if(matrixNorm(matrix_rotation - m1) < matrixNorm(matrix_rotation - m2))
+        {
+            psi = psi1;
+            theta = theta1;
+            phi = phi1;
+        }
+        else
+        {
+            psi = psi2;
+            theta = theta2;
+            phi = phi2;
+        }
     }
     return QVector3D(psi / PI * 180.0, theta / PI * 180.0, phi / PI * 180.0);
 }
