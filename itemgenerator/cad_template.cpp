67531649b26d1fec65f9b35c2a07c8029d5fb8e8
Sebastian Wolf <wolf.git@sme-gmbh.com>
2015-07-01 15:16:39 +0200

    Removed a blank line, that was not necessary

--- itemgenerator/cad_template.cpp
+++ itemgenerator/cad_template.cpp
@@ -66,7 +66,6 @@ QImage CAD_mainul_subul::wizardImage()
     QString imageFileName = fileinfo.baseName();
     imageFileName.prepend(":/itemGraphic/");
     imageFileName.append(".png");
-                    
     image.load(imageFileName, "PNG");
                        
     return image;
