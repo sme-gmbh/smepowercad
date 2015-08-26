bb45768f601a2b26268d945c51fd96bf44913e92
Sebastian Wolf <wolf.git@sme-gmbh.com>
2015-08-26 16:05:36 +0200

    Fixed rotationOfFlange

--- items/cad_basic_duct.cpp
+++ items/cad_basic_duct.cpp
@@ -305,5 +305,17 @@ void CAD_basic_duct::paint(GLWidget *glwidget)
 
 QMatrix4x4 CAD_basic_duct::rotationOfFlange(quint8 num)
 {
-    return matrix_rotation;
+    if(num == 1)
+    {
+        QMatrix4x4 m;
+        m.setToIdentity();
+        m.rotate(180.0, 0.0, 0.0, 1.0);
+        return matrix_rotation * m;
+    }
+    else if(num == 2)
+    {
+        return matrix_rotation;
+    }
+    else
+        return matrix_rotation;
 }
