092954a9b9529bed76fe541b7b09e86a5ded289d
Sebastian Wolf <wolf.git@sme-gmbh.com>
2015-09-08 11:38:24 +0200

    Added Geometry to Sanitary Flange

--- items/cad_sanitary_flange.cpp
+++ items/cad_sanitary_flange.cpp
@@ -25,18 +25,18 @@ CAD_Sanitary_Flange::CAD_Sanitary_Flange() : CADitem(CADitemTypes::Sanitary_Flan
     wizardParams.insert("Angle y", 0.0);
     wizardParams.insert("Angle z", 0.0);
 
-//    arrayBufVertices = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
-//    arrayBufVertices->create();
-//    arrayBufVertices->setUsagePattern(QOpenGLBuffer::StaticDraw);
 
-//    indexBufFaces = new QOpenGLBuffer(QOpenGLBuffer::IndexBuffer);
-//    indexBufFaces->create();
-//    indexBufFaces->setUsagePattern(QOpenGLBuffer::StaticDraw);
+    wizardParams.insert("d",  150.0);
+    wizardParams.insert("fe",  10.0);
+    wizardParams.insert("ff",  40.0);
+    wizardParams.insert("l",  100.0);
+    wizardParams.insert("s",    5.0);
 
-//    indexBufLines = new QOpenGLBuffer(QOpenGLBuffer::IndexBuffer);
-//    indexBufLines->create();
-//    indexBufLines->setUsagePattern(QOpenGLBuffer::StaticDraw);
-   
+    pipe = new CAD_basic_pipe();
+    flange = new CAD_basic_pipe();
+    this->subItems.append(pipe);
+    this->subItems.append(flange);
+
     processWizardInput();
     calculate();
 }
@@ -87,14 +87,50 @@ QString CAD_Sanitary_Flange::description()
 }
 
 void CAD_Sanitary_Flange::calculate()
-{                
+{
+    matrix_rotation.setToIdentity();
+    matrix_rotation.rotate(angle_x, 1.0, 0.0, 0.0);
+    matrix_rotation.rotate(angle_y, 0.0, 1.0, 0.0);
+    matrix_rotation.rotate(angle_z, 0.0, 0.0, 1.0);
+
     boundingBox.reset();
-                    
+
     this->snap_flanges.clear();
     this->snap_center.clear();
     this->snap_vertices.clear();
-                                
+
     this->snap_basepoint = (position);
+
+    pipe->wizardParams.insert("Position x", position.x());
+    pipe->wizardParams.insert("Position y", position.y());
+    pipe->wizardParams.insert("Position z", position.z());
+    pipe->wizardParams.insert("Angle x", angle_x);
+    pipe->wizardParams.insert("Angle y", angle_y);
+    pipe->wizardParams.insert("Angle z", angle_z);
+    pipe->wizardParams.insert("l", l);
+    pipe->wizardParams.insert("d", d);
+    pipe->wizardParams.insert("s",  s);
+    pipe->layer = this->layer;
+    pipe->processWizardInput();
+    pipe->calculate();
+
+    QVector3D position_fl = position + matrix_rotation * QVector3D(l - fe,0.0, 0.0);
+    flange->wizardParams.insert("Position x", position_fl.x());
+    flange->wizardParams.insert("Position y", position_fl.y());
+    flange->wizardParams.insert("Position z", position_fl.z());
+    flange->wizardParams.insert("Angle x", angle_x);
+    flange->wizardParams.insert("Angle y", angle_y);
+    flange->wizardParams.insert("Angle z", angle_z);
+    flange->wizardParams.insert("l", fe);
+    flange->wizardParams.insert("d", d + 2 * ff);
+    flange->wizardParams.insert("s",  ff);
+    flange->layer = this->layer;
+    flange->processWizardInput();
+    flange->calculate();
+
+    this->boundingBox = pipe->boundingBox;
+    this->boundingBox.enterVertices(flange->boundingBox.getVertices());
+    this->snap_flanges = pipe->snap_flanges;
 }
 
 void CAD_Sanitary_Flange::processWizardInput()
@@ -106,12 +142,11 @@ void CAD_Sanitary_Flange::processWizardInput()
     angle_y = wizardParams.value("Angle y").toDouble();
     angle_z = wizardParams.value("Angle z").toDouble();
 
-
-
-    matrix_rotation.setToIdentity();
-    matrix_rotation.rotate(angle_x, 1.0, 0.0, 0.0);
-    matrix_rotation.rotate(angle_y, 0.0, 1.0, 0.0);
-    matrix_rotation.rotate(angle_z, 0.0, 0.0, 1.0);
+    d = wizardParams.value("d").toDouble();
+    ff = wizardParams.value("ff").toDouble();
+    l = wizardParams.value("l").toDouble();
+    fe = wizardParams.value("fe").toDouble();
+    s = wizardParams.value("s").toDouble();
 }
 
 //void CAD_Sanitary_Flange::paint(GLWidget *glwidget)
