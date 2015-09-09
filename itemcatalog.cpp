d61d89005a216cc595a83122e81626f0104f0e31
Peter Diener <diener.git@sme-gmbh.com>
2015-09-09 23:30:11 +0200

    Implemented database version control with git in itemcatalog.

--- itemcatalog.cpp
+++ itemcatalog.cpp
@@ -8,6 +8,9 @@ ItemCatalog::ItemCatalog(ItemDB* itemDB,  ItemWizard* itemWizard, QWidget *paren
     ui->setupUi(this);
     this->itemDB = itemDB;
     this->itemWizard = itemWizard;
+    if (!setupLocalDirectory())
+        return;
+    setupGitProcess();
     initialize();
 }
 
@@ -28,6 +31,113 @@ void ItemCatalog::initialize()
     if (domains.size() > 0)
     {
         on_comboBox_domain_activated(domains.first());
+    }
+}
+
+bool ItemCatalog::setupLocalDirectory()
+{
+    catalogDir = QDir::home();
+    if (!catalogDir.cd(".smepowercad/catalog"))     // local catalog directory does not exist
+    {
+        if (catalogDir.mkpath(".smepowercad/catalog"))
+            ui->textEdit_terminalOutput->append(tr("The local directory .smepowercad/catalog did not exist and has been created."));
+        else
+        {
+            ui->textEdit_terminalOutput->append(tr("The local directory .smepowercad/catalog could not be created!"));
+            return false;
+        }
+    }
+    else
+    {
+        ui->textEdit_terminalOutput->append(tr("The local directory .smepowercad/catalog does exist."));
+    }
+
+    ui->lineEdit_db_localDirectory->setText(catalogDir.canonicalPath());
+    return true;
+}
+
+bool ItemCatalog::setupGitProcess()
+{
+    process_git.setProgram("git");
+    process_git.setProcessChannelMode(QProcess::MergedChannels);
+    process_git.setTextModeEnabled(true);
+    process_git.setWorkingDirectory(catalogDir.canonicalPath());
+
+    QStringList args;
+    args << "--version";
+    process_git.setArguments(args);
+
+    connect(&process_git, SIGNAL(started()), this, SLOT(slot_processGit_started()));
+    connect(&process_git, SIGNAL(readyRead()), this, SLOT(slot_processGit_readyRead()));
+    connect(&process_git, SIGNAL(error(QProcess::ProcessError)), this, SLOT(slot_processGit_error(QProcess::ProcessError)));
+    connect(&process_git, SIGNAL(finished(int,QProcess::ExitStatus)), this, SLOT(slot_processGit_finished(int,QProcess::ExitStatus)));
+    process_git.start();
+    process_git.waitForStarted(1000);
+}
+
+
+void ItemCatalog::slot_processGit_started()
+{
+    ui->pushButton_db_gitClone->setEnabled(false);
+    ui->pushButton_db_gitCommit->setEnabled(false);
+    ui->pushButton_db_gitLog->setEnabled(false);
+    ui->pushButton_db_gitPull->setEnabled(false);
+    ui->pushButton_db_gitPush->setEnabled(false);
+    ui->pushButton_db_gitStatus->setEnabled(false);
+//    ui->textEdit_terminalOutput->append(tr("Git started."));
+}
+
+void ItemCatalog::slot_processGit_finished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+    switch (exitStatus)
+    {
+    case QProcess::NormalExit:
+        if (exitCode == 0)
+            ui->textEdit_terminalOutput->append(tr("Git finished successfully.\n\n"));
+        else
+            ui->textEdit_terminalOutput->append(tr("Git finished unsuccessfully with exit code %1.\n\n").arg(QString().setNum(exitCode)));
+        break;
+    case QProcess::CrashExit:
+        ui->textEdit_terminalOutput->append(tr("Git crashed, exit code %1.\n\n").arg(QString().setNum(exitCode)));
+        break;
+    }
+
+    ui->pushButton_db_gitClone->setEnabled(true);
+    ui->pushButton_db_gitCommit->setEnabled(true);
+    ui->pushButton_db_gitLog->setEnabled(true);
+    ui->pushButton_db_gitPull->setEnabled(true);
+    ui->pushButton_db_gitPush->setEnabled(true);
+    ui->pushButton_db_gitStatus->setEnabled(true);
+}
+
+void ItemCatalog::slot_processGit_readyRead()
+{
+    QByteArray output = process_git.readAll();
+    ui->textEdit_terminalOutput->append(QString::fromUtf8(output));
+}
+
+void ItemCatalog::slot_processGit_error(QProcess::ProcessError error)
+{
+    switch (error)
+    {
+    case QProcess::FailedToStart:
+        ui->textEdit_terminalOutput->append(tr("Git failed to start!"));
+        break;
+    case QProcess::Crashed:
+        ui->textEdit_terminalOutput->append(tr("Git crashed!"));
+        break;
+    case QProcess::Timedout:
+        ui->textEdit_terminalOutput->append(tr("Git timed out!"));
+        break;
+    case QProcess::WriteError:
+        ui->textEdit_terminalOutput->append(tr("Git process write error!"));
+        break;
+    case QProcess::ReadError:
+        ui->textEdit_terminalOutput->append(tr("Git process read error!"));
+        break;
+    case QProcess::UnknownError:
+        ui->textEdit_terminalOutput->append(tr("Git unknown error!"));
+        break;
     }
 }
 
@@ -100,4 +210,86 @@ void ItemCatalog::on_toolButton_removeModel_clicked()
     return;
 
 
+}
+
+void ItemCatalog::on_pushButton_db_gitClone_clicked()
+{
+    QStringList args;
+    args << "clone";
+    process_git.setArguments(args);
+    process_git.start();
+    process_git.waitForStarted(1000);
+}
+
+void ItemCatalog::on_pushButton_db_gitStatus_clicked()
+{
+    QStringList args;
+    args << "push";
+    process_git.setArguments(args);
+    process_git.start();
+    process_git.waitForStarted(1000);
+}
+
+void ItemCatalog::on_pushButton_db_gitLog_clicked()
+{
+    QStringList args;
+    args << "log";
+    process_git.setArguments(args);
+    process_git.start();
+    process_git.waitForStarted(1000);
+}
+
+void ItemCatalog::on_pushButton_db_gitPull_clicked()
+{
+    QStringList args;
+    args << "pull";
+    process_git.setArguments(args);
+    process_git.start();
+    process_git.waitForStarted(1000);
+}
+
+void ItemCatalog::on_pushButton_db_gitCommit_clicked()
+{
+    bool ok;
+    QString commitMessage = QInputDialog::getText(this, tr("Git Commit"), tr("Enter commit message"), QLineEdit::Normal, QString(), &ok);
+    if (!ok)
+        return;
+    if (commitMessage.isEmpty())
+    {
+        QMessageBox::critical(this, tr("Git Commit"), tr("Committing without message is not allowed!\n Commit aborted."));
+        return;
+    }
+
+    QStringList args;
+    args << "add";
+    args << ".";
+    process_git.setArguments(args);
+    process_git.start();
+    process_git.waitForStarted(1000);
+    process_git.waitForFinished(30000);
+
+    args.clear();
+    args << "commit";
+    args << "-m";
+    args << commitMessage;
+    process_git.setArguments(args);
+    process_git.start();
+    process_git.waitForStarted(1000);
+}
+
+void ItemCatalog::on_pushButton_db_gitPush_clicked()
+{
+    QString remote = ui->lineEdit_db_gitRemote->text();
+    if (remote.isEmpty())
+    {
+        QMessageBox::critical(this, tr("Git Clone"), tr("You cannot clone without remote origin!"));
+        return;
+    }
+
+    QStringList args;
+    args << "clone";
+    args << remote;
+    process_git.setArguments(args);
+    process_git.start();
+    process_git.waitForStarted(1000);
 }
--- itemcatalog.h
+++ itemcatalog.h
@@ -7,6 +7,11 @@
 #include <QRegExp>
 #include <QMessageBox>
 #include <QInputDialog>
+#include <QProcess>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QDebug>
 
 #include "itemdb.h"
 #include "itemwizard.h"
@@ -24,6 +29,10 @@ public:
     ~ItemCatalog();
 
 private slots:
+    void slot_processGit_started();
+    void slot_processGit_finished(int exitCode ,QProcess::ExitStatus exitStatus);
+    void slot_processGit_readyRead();
+    void slot_processGit_error(QProcess::ProcessError error);
     void on_comboBox_domain_activated(const QString &arg1);
 
     void on_comboBox_vendor_activated(const QString &arg1);
@@ -38,12 +47,28 @@ private slots:
 
     void on_toolButton_removeModel_clicked();
 
+    void on_pushButton_db_gitClone_clicked();
+
+    void on_pushButton_db_gitStatus_clicked();
+
+    void on_pushButton_db_gitLog_clicked();
+
+    void on_pushButton_db_gitPull_clicked();
+
+    void on_pushButton_db_gitCommit_clicked();
+
+    void on_pushButton_db_gitPush_clicked();
+
 private:
     Ui::ItemCatalog *ui;
     ItemDB* itemDB;
     ItemWizard* itemWizard;
+    QDir catalogDir;
+    QProcess process_git;
 
     void initialize();
+    bool setupLocalDirectory();
+    bool setupGitProcess();
 };
 
 #endif // ITEMCATALOG_H
